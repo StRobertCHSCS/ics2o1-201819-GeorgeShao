08de921a50f6c1f3fe12b4f5d2fa583e22aab69e
George Shao <GeorgeShao123@gmail.com>
2019-05-14 14:10:35 -0400

    Added up/down/left/right movement to the circle. (previously only up movement)

--- SimpleArcade/player_movement.py
+++ SimpleArcade/player_movement.py
@@ -16,9 +16,15 @@ right_pressed = False
 
 
 def on_update(delta_time):
-    global up_pressed, player_y
+    global up_pressed, down_pressed, left_pressed, right_pressed, player_y, player_x
     if up_pressed:
         player_y += 5
+    if down_pressed:
+        player_y -= 5
+    if left_pressed:
+        player_x -= 5
+    if right_pressed:
+        player_x += 5
 
 
 def on_draw():
@@ -30,14 +36,32 @@ def on_draw():
 
 def on_key_press(key, modifiers):
     global up_pressed
+    global down_pressed
+    global left_pressed
+    global right_pressed
     if key == arcade.key.W:
         up_pressed = True
+    if key == arcade.key.A:
+        left_pressed = True
+    if key == arcade.key.S:
+        down_pressed = True
+    if key == arcade.key.D:
+        right_pressed = True
 
 
 def on_key_release(key, modifiers):
     global up_pressed
+    global down_pressed
+    global left_pressed
+    global right_pressed
     if key == arcade.key.W:
         up_pressed = False
+    if key == arcade.key.A:
+        left_pressed = False
+    if key == arcade.key.S:
+        down_pressed = False
+    if key == arcade.key.D:
+        right_pressed = False
 
 
 def on_mouse_press(x, y, button, modifiers):
